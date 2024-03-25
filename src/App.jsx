9cb658d24bc83acda1449d59f3215617b96c3a4a
thkiaAdiba <athkiaadiba@gmail.com>
2024-03-25 20:20:01 +0600

    added setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-active btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
